4867ae937c96e9d6acf4d645b818c551c5ad0055
bernard-xl <pohshieliang91@gmail.com>
2016-12-20 19:17:32 +0800

    add trie

---
+++ src/main/java/xl/playground/algo/tree/Trie.java
@@ -0,0 +1,165 @@
+package xl.playground.algo.tree;
+
+import java.util.ArrayList;
+
+/**
+ * Created by xl on 12/20/16.
+ */
+public class Trie {
+
+    private static final int ALPHABET_COUNT = 256;
+
+    private Node root;
+
+    public void put(String word) {
+        root = internalPut(root, word, 0);
+    }
+
+    public void delete(String word) {
+        root = internalDelete(root, word, 0);
+    }
+
+    public boolean contains(String word) {
+        return internalSearch(root, word) != null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public String longestPrefixOf(String word) {
+        int prefixLength = internalPrefixLength(root, word);
+        return word.substring(0, prefixLength);
+    }
+
+    public Iterable<String> wordsWithPrefix(String prefix) {
+        Node node = internalSearch(root, prefix);
+        ArrayList<String> result = new ArrayList<>();
+        internalPrefixCollect(node, "", result);
+        return result;
+    }
+
+    public Iterable<String> wordsThatMatch(String pattern) {
+        ArrayList<String> result = new ArrayList<>();
+        internalMatchCollect(root, pattern, "", result);
+        return result;
+    }
+
+    public Iterable<String> words() {
+        ArrayList<String> result = new ArrayList<>();
+        internalPrefixCollect(root, "", result);
+        return result;
+    }
+
+    private Node internalPut(Node root, String word, int i) {
+        if (root == null) {
+            root = new Node();
+        }
+
+        if (i == word.length()) {
+            root.word = true;
+            return root;
+        }
+
+        char c = word.charAt(i);
+        root.children[c] = internalPut(root.children[c], word, ++i);
+
+        return root;
+    }
+
+    private Node internalDelete(Node root, String word, int i) {
+        if (root == null) {
+            return null;
+        }
+
+        if (i == word.length()) {
+            root.word = false;
+        } else {
+            char c = word.charAt(i);
+            root.children[c] = internalDelete(root.children[c], word, ++i);
+        }
+
+        if (root.word) {
+            return root;
+        }
+
+        for (int j = 0; i < ALPHABET_COUNT; j++) {
+            if (root.children[j] != null) return root;
+        }
+        return null;
+    }
+
+    private Node internalSearch(Node root, String word) {
+        if (root == null) {
+            return null;
+        }
+
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (root.children[c] == null) {
+                return null;
+            }
+            root = root.children[c];
+        }
+
+        return root;
+    }
+
+    private int internalPrefixLength(Node root, String word) {
+        if (root == null) {
+            return 0;
+        }
+
+        int result = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (root.children[c] == null) break;
+            result += 1;
+            root = root.children[c];
+        }
+
+        return result;
+    }
+
+    private void internalPrefixCollect(Node root, String prefix, ArrayList<String> result) {
+        if (root == null) {
+            return;
+        }
+
+        for (char i = 0; i < ALPHABET_COUNT; i++) {
+            Node child = root.children[i];
+            if (child != null) {
+                if (child.word) {
+                    result.add(prefix + i);
+                }
+                internalPrefixCollect(child, prefix + i, result);
+            }
+        }
+    }
+
+    private void internalMatchCollect(Node root, String pattern, String prefix, ArrayList<String> result) {
+        if (root == null) {
+            return;
+        }
+
+        if (prefix.length() == pattern.length()) {
+            if (root.word) result.add(prefix);
+            return;
+        }
+
+        char p = pattern.charAt(prefix.length());
+        if (p == '.') {
+            for (char i = 0; i < ALPHABET_COUNT; i++) {
+                internalMatchCollect(root.children[i], pattern, prefix + i, result);
+            }
+        } else {
+            internalMatchCollect(root.children[p], pattern, prefix + p, result);
+        }
+    }
+
+    private static class Node {
+
+        boolean word = false;
+        Node[] children = new Node[ALPHABET_COUNT];
+    }
+}
