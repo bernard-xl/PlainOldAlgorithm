8ff1706101132410f992bd4c95470fa920d556c0
shieliang <poh_s@worksap.co.jp>
2016-12-23 12:34:20 +0800

    add treap test and benchmark

--- src/main/java/xl/playground/algo/tree/Treap.java
+++ src/main/java/xl/playground/algo/tree/Treap.java
@@ -1,6 +1,7 @@
 package xl.playground.algo.tree;
 
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by xl on 12/22/16.
@@ -19,8 +20,51 @@ public class Treap<Item extends Comparable<Item>> {
         root = removeNode(root, value);
     }
 
-    public boolean isBalanced() {
-        return false;
+    public int size() {
+        return (root == null) ? 0 : root.count;
+    }
+
+    public int depth() {
+        return (root == null) ? 0 : root.level;
+    }
+
+    public int balance() {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = (root.left == null) ? 0 : root.left.level;
+        int right = (root.right == null) ? 0 : root.right.level;
+
+        return left - right;
+    }
+
+    public boolean isBinarySearchTree() {
+        return checkBinarySearchTreeRule(root);
+    }
+
+    @Override
+    public String toString() {
+        ArrayList<List<Item>> levels = new ArrayList<>();
+        List<Node<Item>> last = Collections.singletonList(root);
+
+        while (last.stream().anyMatch(Objects::nonNull)) {
+            levels.add(last.stream().map(i -> i != null ? i.value : null).collect(Collectors.toList()));
+
+            ArrayList<Node<Item>> current = new ArrayList<>(last.size() * 2);
+            for (Node<Item> item : last) {
+                if (item == null) {
+                    current.add(null);
+                    current.add(null);
+                } else {
+                    current.add(item.left);
+                    current.add(item.right);
+                }
+            }
+            last = current;
+        }
+
+        return levels.toString();
     }
 
     private Node<Item> addNode(Node<Item> root, Item value) {
@@ -41,6 +85,7 @@ public class Treap<Item extends Comparable<Item>> {
         }
 
         root.count = countChildren(root);
+        root.level = countLevel(root);
         return root;
     }
 
@@ -58,7 +103,10 @@ public class Treap<Item extends Comparable<Item>> {
             root.right = removeNode(root.right, value);
         }
 
-        root.count = countChildren(root);
+        if (root != null) {
+            root.count = countChildren(root);
+            root.level = countLevel(root);
+        }
         return root;
     }
 
@@ -74,7 +122,9 @@ public class Treap<Item extends Comparable<Item>> {
                 root = rotateLeft(root);
                 root.left = sinkNode(root.left);
             }
+
             root.count = countChildren(root);
+            root.level = countLevel(root);
             return root;
         }
 
@@ -98,13 +148,34 @@ public class Treap<Item extends Comparable<Item>> {
     private int countChildren(Node<Item> root) {
         int left = (root.left == null) ? 0 : root.left.count;
         int right = (root.right == null) ? 0 : root.right.count;
-        return left + right;
+        return 1 + left + right;
+    }
+
+    private int countLevel(Node<Item> root) {
+        int left = (root.left == null) ? 0 : root.left.level;
+        int right = (root.right == null) ? 0 : root.right.level;
+        return 1 + Math.max(left, right);
+    }
+
+    private boolean checkBinarySearchTreeRule(Node<Item> root) {
+        if (root == null) {
+            return true;
+        }
+
+        boolean leftOk = root.left == null ||
+                (root.left.value.compareTo(root.value) <= 0 && checkBinarySearchTreeRule(root.left));
+
+        boolean rightOk = root.right == null ||
+                (root.right.value.compareTo(root.value) > 0 && checkBinarySearchTreeRule(root.right));
+
+        return leftOk && rightOk;
     }
 
     private static class Node<Item extends Comparable<Item>> {
 
         int rank;
         int count;
+        int level;
         Item value;
         Node<Item> left;
         Node<Item> right;
@@ -112,7 +183,40 @@ public class Treap<Item extends Comparable<Item>> {
         public Node(int rank, Item value) {
             this.rank = rank;
             this.count = 1;
+            this.level = 1;
             this.value = value;
         }
+    }
+
+    public static void main(String... args) {
+        Treap<Integer> treap = new Treap<>();
+
+        System.out.println("Adding....\n");
+
+        for (int i = 0; i < 4096; i++) {
+            treap.add(i);
+            if (!treap.isBinarySearchTree()) {
+                throw new IllegalStateException("treap violate binary search tree rule");
+            }
+        }
+        printTreap(treap);
+
+        System.out.println("\nDeleting....\n");
+
+        for (int i = 0; i < 4096; i++) {
+            treap.remove(i);
+            if (!treap.isBinarySearchTree()) {
+                throw new IllegalStateException("treap violate binary search tree rule");
+            }
+        }
+        printTreap(treap);
+    }
+
+    private static void printTreap(Treap<?> treap) {
+        System.out.println("size         : " + treap.size());
+        System.out.println("depth        : " + treap.depth() + " (optimal is " + (int) (Math.log(treap.size()) + 1.5) + ")");
+        System.out.println("balance      : " + treap.balance());
+        // System.out.println("internal view: " + treap);
+        System.out.println();
     }
 }
